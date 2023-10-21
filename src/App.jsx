21b234635ce21a5296dbaee92e8c55fa975e8717
Andrea Lopez <skarletswdev@gmail.com>
2023-10-21 14:50:30 -0300

    boton incrementar 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import { useState } from 'react'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count, setCount] = useState(0);
+
 
   return (
-    <></>
+    <>
+      <p>Contador {count}</p>
+      <button onClick={() => setCount(count + 1)}>Incrementar</button>
+    </>
   )
 }
 
