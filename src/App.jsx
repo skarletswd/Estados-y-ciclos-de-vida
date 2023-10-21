9efda7f9ae161e8b0655805887fb78b0af2b8003
Andrea Lopez <skarletswdev@gmail.com>
2023-10-21 18:33:21 -0300

    useEffect MinMax

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
 import { useState, useEffect } from 'react'
+import MinMax from './MinMax'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
 
-  const [count, setCount] = useState(0);
+    return (
+      <MinMax />
+    )
+  // const [count, setCount] = useState(0);
 
-  const decrementar = () => {
-    if (count > 0) {
-      setCount(count -1);
-    }
+  // const decrementar = () => {
+  //   if (count > 0) {
+  //     setCount(count -1);
+  //   }
     
-  };
+  // };
 
-  useEffect(()=> {
-    console.log("useEffect")
-  }, [count]);
+  // useEffect(()=> {
+  //   console.log("useEffect")
+  // }, [count]);
 
 
-  return (
-    <>
-      <p>Contador {count}</p>
-      <button onClick={() => setCount(count + 1)}>Incrementar</button>
-      <button onClick={decrementar}>Decrementar</button>
-    </>
-  )
+  // return (
+  //   <>
+  //     <p>Contador {count}</p>
+  //     <button onClick={() => setCount(count + 1)}>Incrementar</button>
+  //     <button onClick={decrementar}>Decrementar</button>
+  //   </>
+  // )
 };
 
 export default App
