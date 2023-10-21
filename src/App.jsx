0622617628c98dcc3790c7727aa0cbd7694e6288
Andrea Lopez <skarletswdev@gmail.com>
2023-10-21 15:11:08 -0300

    boton decrementar con una funcion

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,16 @@ function App() {
 
   const [count, setCount] = useState(0);
 
+  const decrementar = () => {
+    setCount(count -1)
+  }
+
 
   return (
     <>
       <p>Contador {count}</p>
       <button onClick={() => setCount(count + 1)}>Incrementar</button>
+      <button onClick={decrementar}>Decrementar</button>
     </>
   )
 }
